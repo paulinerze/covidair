4508681e1246887e756b8d91ac509df73023ad5d
pauline rouze <rouzepau@gmail.com>
2020-04-25 12:17:53 +0200

    ajout map

--- app/src/main/java/com/miage/covidair/MapActivity.java
+++ app/src/main/java/com/miage/covidair/MapActivity.java
@@ -1,52 +1,38 @@
 package com.miage.covidair;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.miage.covidair.event.EventBusManager;
 import com.miage.covidair.event.SearchCityResultEvent;
-import com.miage.covidair.model.City.City;
 import com.miage.covidair.service.CitySearchService;
-import com.miage.covidair.adapter.CityAdapter;
 import com.squareup.otto.Subscribe;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    @BindView(R.id.recyclerView)
-    RecyclerView mRecyclerView;
-    private CityAdapter mCityAdapter;
-    @BindView(R.id.activity_main_loader)
-    ProgressBar mProgressBar;
-
+    private GoogleMap mActiveGoogleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_map);
 
         // Binding ButterKnife annotations now that content view has been set
         ButterKnife.bind(this);
 
-        // Instanciate a CityAdpater with empty content
-        mCityAdapter = new CityAdapter(this, new ArrayList<>());
-        mRecyclerView.setAdapter(mCityAdapter);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // Get map fragment
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -56,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Register to Event bus : now each time an event is posted, the activity will receive it if it is @Subscribed to this event
         EventBusManager.BUS.register(this);
-
         CitySearchService.INSTANCE.searchCities();
     }
 
@@ -65,20 +50,15 @@ public class MainActivity extends AppCompatActivity {
         // Unregister from Event bus : if event are posted now, the activity will not receive it
         EventBusManager.BUS.unregister(this);
 
+        // Do NOT forget to call super.onPause()
         super.onPause();
     }
 
     @Subscribe
     public void searchResult(final SearchCityResultEvent event) {
-        // Here someone has posted a SearchCityResultEvent
-        // Update adapter's model
-        runOnUiThread(() -> {
-            mCityAdapter.setCities(event.getCities());
-            mCityAdapter.notifyDataSetChanged();
-            mProgressBar.setVisibility(View.GONE);
-        });
-        mCityAdapter.setCities(event.getCities());
-        runOnUiThread(() -> mCityAdapter.notifyDataSetChanged());
+        // Here someone has posted a SearchResultEvent
+
+        // Update map's markers
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -89,15 +69,22 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_list:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
                 return true;
             case R.id.action_favorite:
                 /* DO ADD */
                 return true;
             case R.id.action_map:
-                Intent intent = new Intent(this, MapActivity.class);
-                startActivity(intent);
+                //Intent intent = new Intent(this, MapActivity.class);
+                //startActivity(intent);
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mActiveGoogleMap = googleMap;
     }
 }
