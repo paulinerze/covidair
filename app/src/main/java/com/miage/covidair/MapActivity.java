3f1121844f51f1596e898600cffa6ecf8e5a5760
pauline rouze <rouzepau@gmail.com>
2020-04-29 11:25:39 +0200

    ajout infowindow map

--- app/src/main/java/com/miage/covidair/MapActivity.java
+++ app/src/main/java/com/miage/covidair/MapActivity.java
@@ -19,10 +19,13 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.miage.covidair.event.EventBusManager;
 import com.miage.covidair.event.SearchCityResultEvent;
+import com.miage.covidair.event.SearchLocationResultEvent;
 import com.miage.covidair.model.City.City;
+import com.miage.covidair.model.Location.Location;
 import com.miage.covidair.service.CitySearchService;
 import com.miage.covidair.service.LocationSearchService;
 import com.squareup.otto.Subscribe;
@@ -30,7 +33,7 @@ import com.squareup.otto.Subscribe;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     private GoogleMap mActiveGoogleMap;
     @BindView(R.id.activity_main_loader)
@@ -109,17 +112,80 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                     for (City city : event.getCities()) {
                         // Step 1: create marker icon (and resize drawable so that marker is not too big)
                         if (city.latitude != null && city.longitude != null){
-                            int markerIconResource;
-                            markerIconResource = R.drawable.baseline_location_on_black_18dp;
-                            Bitmap imageBitmap = BitmapFactory.decodeResource(getResources(), markerIconResource);
-                            Bitmap resizedBitmap = Bitmap.createScaledBitmap(imageBitmap, 50, 50, false);
-
                             // Step 2: define marker options
                             MarkerOptions markerOptions = new MarkerOptions()
                                     .position(new LatLng(Double.valueOf(city.getLatitude()), Double.valueOf(city.getLongitude())))
-                                    .title(city.name)
-                                    .snippet("Nombre de mesures : " +city.count)
-                                    .icon(BitmapDescriptorFactory.fromBitmap(resizedBitmap));
+                                    .title(city.name);
+
+                            // Step 3: add marker
+                            mActiveGoogleMap.addMarker(markerOptions);
+                        }
+                    }
+                }
+            }
+        });
+
+    }
+
+    @Subscribe
+    public void searchLocationResult(final SearchLocationResultEvent event) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+
+                // Here someone has posted a SearchResultEvent
+                // Check that map is ready
+                if (mActiveGoogleMap != null) {
+                    // Update map's markers
+                    mActiveGoogleMap.clear();
+                    for (Location location : event.getLocations()) {
+                        // Step 1: create marker icon (and resize drawable so that marker is not too big)
+                        if (location.latitude != null && location.longitude != null){
+                            // Step 2: define marker options
+                            String measurements = "";
+                            if (location.getLatestMeasurements() != null && !location.getLatestMeasurements().isEmpty()){
+                                String bc = "";
+                                String co = "";
+                                String no2 = "";
+                                String o3 = "";
+                                String pm10 = "";
+                                String pm25 = "";
+                                String so2 = "";
+
+                                if (location.getLatestMeasurements().containsKey("bc")){
+                                    bc = "BC : " + location.getLatestMeasurements().get("bc").value + " " + location.getLatestMeasurements().get("bc").unit + "\n";
+                                }
+                                if (location.getLatestMeasurements().containsKey("co")){
+                                    co = "CO : " + location.getLatestMeasurements().get("co").value + " " + location.getLatestMeasurements().get("co").unit + "\n";
+                                }
+                                if (location.getLatestMeasurements().containsKey("no2")){
+                                    no2 = "NO2 : " + location.getLatestMeasurements().get("no2").value + " " + location.getLatestMeasurements().get("no2").unit + "\n";
+                                }
+                                if (location.getLatestMeasurements().containsKey("o3")){
+                                    o3 = "O3 : " + location.getLatestMeasurements().get("o3").value + " " + location.getLatestMeasurements().get("o3").unit + "\n";
+                                }
+                                if (location.getLatestMeasurements().containsKey("pm10")){
+                                    pm10 = "PM10 : " + location.getLatestMeasurements().get("pm10").value + " " + location.getLatestMeasurements().get("pm10").unit + "\n";
+                                }
+                                if (location.getLatestMeasurements().containsKey("pm25")){
+                                    pm25 = "PM25 : " + location.getLatestMeasurements().get("pm25").value + " " + location.getLatestMeasurements().get("pm25").unit + "\n";
+                                }
+                                if (location.getLatestMeasurements().containsKey("so2")){
+                                    so2 = "SO2 : " + location.getLatestMeasurements().get("so2").value + " " + location.getLatestMeasurements().get("so2").unit + "\n";
+                                }
+
+                                measurements = bc + co + no2 + o3 + pm10 + pm25 + so2;
+                            }
+
+                            String temperature = "";
+                            if ( location.sol != null ){
+                                temperature = "Température actuelle : " + location.sol + "°C \n";
+                            }
+                            MarkerOptions markerOptions = new MarkerOptions()
+                                    .position(new LatLng(Double.valueOf(location.getLatitude()), Double.valueOf(location.getLongitude())))
+                                    .title(location.location)
+                                    .snippet(temperature + measurements);
+                                    //.icon(BitmapDescriptorFactory.fromBitmap(resizedBitmap));
 
                             // Step 3: add marker
                             mActiveGoogleMap.addMarker(markerOptions);
@@ -157,5 +223,15 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mActiveGoogleMap = googleMap;
+        mActiveGoogleMap.setOnInfoWindowClickListener(WhenInfoWindowClick);
     }
+
+    GoogleMap.OnInfoWindowClickListener WhenInfoWindowClick = new GoogleMap.OnInfoWindowClickListener(){
+        @Override
+        public void onInfoWindowClick(Marker marker) {
+            mActiveGoogleMap.clear();
+            LocationSearchService.INSTANCE.searchLocations(marker.getTitle());
+
+        }
+    };
 }
